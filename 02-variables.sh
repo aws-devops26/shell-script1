12776001928204c4d513fe708cba0bed940daff6
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-14 19:55:58 +0530

    added 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "sri : hello venkat g.m"
-echo "venkat: hi sri very g.m"
-echo "sri : how r u venkat"
-echo "venkat:i m good sri...how about u" 
+per1=sri
+per2=venkat
+echo "$per1 : hello $per2 g.m"
+echo "$per2: hi $per1 very g.m"
+echo "$per1 : how r u $per2"
+echo "$per2:i m good $per1...how about u" 
