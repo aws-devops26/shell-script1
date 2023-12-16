e019bb97ca6e1d74be6b7a6b8aa613f4f7152649
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-16 14:37:56 +0530

    checking conditions

--- 10-logs.sh
+++ 10-logs.sh
@@ -5,8 +5,8 @@ R="\e[31m"
 N="\e[0m"
 G="\e[32m"
 Y="\e[33m"
-logfile= "/tmp/$0-$TIMESTAMP.log"
-echo " script started executed at $TIMESTAMP" &>> $logfile
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo " script started executed at $TIMESTAMP" &>> $LOGFILE
 validate(){
 if [ $1 -ne 0 ]
 then
@@ -23,7 +23,7 @@ exit 5
 else
 echo  -e " $G u r root user $N"
 fi
-yum install mysql -y &>> $logfile
+yum install mysql -y &>> $LOGFILE
 validate $? -e " $G mysql installed $N "
-yum install git -y &>> $logfile
+yum install git -y &>> $LOGFILE
 validate $? -e " $G git installed $N "
