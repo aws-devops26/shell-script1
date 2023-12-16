446107ebe875a2c1b51f219645bc6ab5b0749599
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-16 13:57:20 +0530

    checking conditions

---
+++ 09-function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+id=$(id -u)
+R="\e[31m"
+N="\e[0m"
+G="\e[32m"
+validate(){
+if [ $id -ne 0 ]
+then
+echo  -e " $R installing my sql FAILED $N"
+exit 5
+else
+echo  -e " $G installing my sql SUCESS $N"
+fi
+}
+if [ $id -ne 0 ]
+then
+echo  -e " $R error : please run this script with root access $N"
+exit 5
+else
+echo  -e " $G u r root user $N"
+fi
+yum install mysql -y
+validate
