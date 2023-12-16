ad2c08a600770efb5a7c1e81a7e65c263b5c877c
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-16 13:36:32 +0530

    checking conditions

--- 08-installmysql.sh
+++ 08-installmysql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 id=$(id -u)
-r= "\e[31m"
-n= "\e[0m"
-g= "\e[32m"
+r="\e[31m"
+n="\e[0m"
+g="\e[32m"
 if [ $id -ne 0 ]
 then
 echo " $r error : please run this script with root access $n"
