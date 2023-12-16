e9eddafcadaa1b2422670a023a6ce9587388ece1
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-16 16:16:07 +0530

    checking conditions

--- 11-installpkgs.sh
+++ 11-installpkgs.sh
@@ -6,13 +6,13 @@ N="\e[0m"
 Y="\e[33m"
  VALIDATE()
 {
-if [ $1 -ne 0 ]
-then
-echo  -e " $R $2.... FAILED $N"
-exit 5
-else
-echo  -e " $G $1.... SUCESS $N"
-fi
+ if [ $1 -ne 0 ]
+    then
+    echo  -e " $R $2.... FAILED $N"
+    exit 5
+    else
+    echo  -e " $G $1.... SUCESS $N"
+ fi
 }
 if [ $id -ne 0 ]
     then
@@ -21,16 +21,18 @@ if [ $id -ne 0 ]
     else
     echo -e " $G u r root user $N"
 fi
+
 echo  "all arguements passed:$@ "
 
 for package in $@
 do
     yum list installed $package
-    if [ $? -ne 0 ]
+ if [ $? -ne 0 ]
     then
     yum install $package -y
     VALIDATE $? "installation of $packages "
  else
     echo -e "$Y $package is already installed....$R SKIPPING $N"
+ fi
 
 done
