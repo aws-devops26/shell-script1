36f3430cf9e19e4982cc33b77c0a1def4fe0df3b
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-19 23:15:22 +0530

    deleting old logs

---
+++ 14-diskusg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DISK_USAGE=$(df -hT | grep -VE 'tmp|file')
+DISK_THRESHOLD=1
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print 6f}' |cut -d % -f1)
+    partition=$(echo $line | awk '{print $1f}')
+    if [ $usage -ge $DISK_THRESHOLD ]
+        then
+        message+= " high disk usage on $partion: $usage \n"
+    fi
+done <<< $DISK_USAGE
+    echo -e "message: $message"
+    echo " $message" | mail -s "high disk usage" 
