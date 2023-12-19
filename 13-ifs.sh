4aa6c6ae1d9d5af114fa2bf40553a3ec2f87e887
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-19 19:27:33 +0530

    deleting old logs

---
+++ 13-ifs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+file=/etc/passwd
+R="\e[31m"
+N="\e[0m"
+if [ ! -f $file ]
+    then
+    echo -e " $R source diectory : $SOURCE_DIR does not exists. $N"
+fi 
+while IFS=":" rear -r username password user-id group-id user-fullname home-dir shell-path
+do  
+    echo " username : $username"
+    echo " userid = $user-id"
+    echo " user fullname - $user-fullname
+done < $file
