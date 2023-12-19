eb3aa6eee858e04dadc26a746b5b7b820472c84f
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-19 18:02:42 +0530

    deleting old logs

--- 12-deleteoldlogs.sh
+++ 12-deleteoldlogs.sh
@@ -4,7 +4,7 @@ if [ ! -d $SOURCE_DIR ]
 then 
 echo -e " $R source directory : $SOURCE_DIR does not exist. $N "
 fi
-FILES_TO_DELETE=$ (find $SOURCE_DIR  -type f -mtime +14 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR  -type f -mtime +14 -name "*.log")
 while IFS =read -r line 
 do
 echo "deleting file : $line"
