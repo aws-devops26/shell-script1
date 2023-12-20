f2d6e5c35f084eb286db18f54840da0be6770bca
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-20 23:44:40 +0530

    gmail from linux

--- 14-diskusg.sh
+++ 14-diskusg.sh
@@ -4,8 +4,9 @@ DISK_THRESHOLD=1
 message=""
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print $6f}' |cut -d % -f1)
+    
     partition=$(echo $line | awk '{print $1f}')
+    usage=$(echo $line | awk '{print $6f}' |cut -d % -f1)
     if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="high disk usage on $partition: $usage\n"
