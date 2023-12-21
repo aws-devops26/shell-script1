488c6a4736f04d0dc71ec2679d0101b748f760ee
aws-devops26 <srivalli26.najara@gmail.com>
2023-12-21 13:02:40 +0530

    script

--- 14-diskusg.sh
+++ 14-diskusg.sh
@@ -4,9 +4,8 @@ DISK_THRESHOLD=1
 message=" "
 while IFS= read line
 do
-    
-    partition=$(echo $line | awk '{print $1f}')
     usage=$(echo " $line" | awk '{print $6f}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1f}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+=" high disk usage on $partition: $usage\n"
